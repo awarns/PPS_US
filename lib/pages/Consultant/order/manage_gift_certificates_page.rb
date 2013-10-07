fe391d4a22131634a50bd64617fae68128307e97
awarns <itqa1@MAC14958.local>
2013-10-07 17:22:33 -0400

    Gift Cert Done

--- lib/pages/Consultant/order/manage_gift_certificates_page.rb
+++ lib/pages/Consultant/order/manage_gift_certificates_page.rb
@@ -7,6 +7,7 @@ class ManageGiftCertificatesPage
   button(:go, :value => "Go")
   select_list(:status, :id => "MasterContentBody1_content_rep_ddl_gc_status")
   select_list(:lookup_by, :id => "MasterContentBody1_content_rep_ddl_lookupby")
+  table(:gift_cert_table, :id => "MasterContentBody1_content_rep_dg_gc")
 
 
 
@@ -15,14 +16,24 @@ class ManageGiftCertificatesPage
     populate_page_with data_for(:gift_cert_status, data)
     go
     sleep(3)
-    gift_cert_table_element[1][3].should == "$25.00"
+    gift_cert_table_element[1][3].text.should == "$25.00"
 
   end
 
-  def get_cert_number
+  def get_cert_number(data ={})
 
-    @params = gift_cert_table_element[1][0].text
-    puts @params
+    populate_page_with data_for(:gift_cert_status, data)
+    go
+    sleep(3)
+
+
+  end
+
+  def verify_redeemed_card(giftcertnum,data ={})
+
+    populate_page_with data_for(:gift_cert_status, data)
+    gift_cert_table.should =~ /.*#{giftcertnum}.*/
+
 
   end
 
