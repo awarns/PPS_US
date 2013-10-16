54f124c88b7afbb0c99ba623675c559e3a0faefe
awarns <itqa1@MAC14958.local>
2013-10-15 21:39:21 -0400

    Merge branch 'development'

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,6 +5,14 @@ Before do
 
 end
 
-After do
+
+After do |scenario|
+
+  if scenario.failed?
+    filename = "error-#{@current_page.class}-#{Time.now}.png"
+    @current_page.save_screenshot(filename)
+    embed(filename, 'image/png')
+
+  end
   @browser.close
 end
